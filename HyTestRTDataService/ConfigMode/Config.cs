f9f6319f0e503d4065dcc7f4782467b8e5171876
liuyuxinzju <21725152@zju.edu.cn>
2019-06-08 23:39:07 +0800

    彻底对项目重命名

--- HyTestRTDataService/ConfigMode/Config.cs
+++ HyTestRTDataService/ConfigMode/Config.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using HyTestIEInterface;
 
 namespace HyTestRTDataService.ConfigMode
 {
-    public class ConfigAdapter
+    public class Config
     {
-        IAdapterLoader loader;
 
     }
 }
