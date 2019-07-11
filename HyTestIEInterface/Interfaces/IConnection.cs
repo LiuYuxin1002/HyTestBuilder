a5c9d37ea079850606a9031569d7b2421fe0a346
liuyuxinzju <21725152@zju.edu.cn>
2019-07-11 14:20:44 +0800

    清除TCADS部分

--- HyTestIEInterface/Interfaces/IConnection.cs
+++ HyTestIEInterface/Interfaces/IConnection.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TwinCAT.Ads;
 
 namespace HyTestIEInterface
 {
@@ -27,10 +22,5 @@ namespace HyTestIEInterface
         /// </summary>
         /// <returns></returns>
         int disconnect();
-    }
-
-    class Test
-    {
-        TcAdsClient client;
     }
 }
